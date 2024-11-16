7f36be153189609ab01e4c44e095904341ccde68
RedSotecc <camk070220hmchrva4@soycecytem.mx>
2024-11-15 23:59:46 -0600

    Update js.js

--- js.js
+++ js.js
@@ -1,22 +1,17 @@
 window.addEventListener('resize', function () {
-    // Ajustar el tamaño de los íconos al cambiar el tamaño de la ventana
     ajustarIconos();
 });
 
 function ajustarIconos() {
     const iconos = document.querySelectorAll('.menu .icono');
-
-    // Obtén el ancho de la ventana
     const anchoVentana = window.innerWidth;
 
     if (anchoVentana <= 768) {
-        // En dispositivos móviles, hacemos los íconos más pequeños
         iconos.forEach(icono => {
             icono.style.width = '30px';
             icono.style.height = '30px';
         });
     } else {
-        // En pantallas grandes, los íconos tienen un tamaño mayor
         iconos.forEach(icono => {
             icono.style.width = '35px';
             icono.style.height = '35px';
@@ -24,10 +19,7 @@ function ajustarIconos() {
     }
 }
 
-// Llamar la función al cargar la página para asegurarse de que los iconos están ajustados
-ajustarIconos();
-
-// Función para cambiar de sección
+// Función para mostrar la sección seleccionada
 function mostrarSeccion(seccionId) {
     const secciones = document.querySelectorAll('.contenido');
     secciones.forEach(seccion => {
@@ -39,3 +31,5 @@ function mostrarSeccion(seccionId) {
         seccionSeleccionada.style.display = 'block';
     }
 }
+
+ajustarIconos(); // Ajuste inicial de los íconos
